8265ea5c132d458dc13ce6e4d2e94aa72f10be90
2a6m <maxime.bourguignon1996@gmail.com>
2019-03-15 12:01:39 +0100

    look cores's number

--- threads/example.c
+++ threads/example.c
@@ -3,6 +3,7 @@
 #include<pthread.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<sys/sysinfo.h>
 
 pthread_t tid[2];
 
@@ -11,6 +12,7 @@ void* doSomeThing(void *arg)
     unsigned long i = 0;
     pthread_t id = pthread_self();
 
+    /*
     if(pthread_equal(id,tid[0]))
     {
         printf("\n First thread processing\n");
@@ -21,6 +23,8 @@ void* doSomeThing(void *arg)
         printf("\n Second thread processing\n");
         printf("\n %d \n", getpid());
     }
+    */
+    printf("\n N° process: %d \n",getpid());
 
     for(i=0; i<(0xFFFFFFFF);i++);
 
@@ -31,16 +35,20 @@ int main(void)
 {
     int i = 0;
     int err;
+    // use import <sys/sysinfo.h>
+    // it return the number of cores available
+    int nthread = get_nprocs(); 
 
-    while(i < 2)
+    printf("%d", nthread);
+	    
+    while(i < nthread)
     {
         err = pthread_create(&(tid[i]), NULL, &doSomeThing, NULL);
         if (err != 0)
             printf("\ncan't create thread :[%s]", strerror(err));
         else
             printf("\n Thread created successfully\n");
-
-        i++;
+	printf("\n N° threads : %d", ++i);
     }
 
     sleep(5);
